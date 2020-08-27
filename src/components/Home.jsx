2184ba4c41d78882c01df3d1495f1fa0390b88fe
Alex Cruz <alexc017@yahoo.com>
2020-08-27 12:12:35 -0700

    Added Home Component

---
+++ src/components/Home.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import {faHandshake }from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+const Home = () => {
+  return (
+    <section className="home">
+      <div className="homeContent">
+        <h1 className="header">Hello, My name is Alex Cruz</h1>
+        <p className="description">
+          I'am a Self Taught Front-End Web Developer.
+        </p>
+        <a href="#contactMe">
+                  <button className="hireBTN"><FontAwesomeIcon icon={faHandshake}/>HIRE ME</button>
+        </a>
+      </div>
+    </section>
+  );
+};
+
+export default Home;
