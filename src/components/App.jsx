79e1d674f5521f1e614edbdc3163bbd5fdc57dd1
Alex Cruz <alexc017@yahoo.com>
2020-08-27 12:13:35 -0700

    Added images folder

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import Navbar from "./Navbar.jsx"
-
+import Home from "./Home.jsx"
 function App() {
   return (
     <div className="whole-page">
       <Navbar />
+      <Home />
     </div>
   );
 }
