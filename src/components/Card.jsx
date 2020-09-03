b634c24d2e1310a8d610102444130131b471d30a
Alex Cruz <alexc017@yahoo.com>
2020-09-02 23:18:26 -0700

    The style of the website is about 80%finish

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,29 +1,32 @@
 import React from "react";
-import  WeatherApp  from "../videos/Weather-App.gif"
-const Card = (props) => {
-console.log(props.video)
+import WeatherApp from "../videos/Weather-App.gif";
+import { faInfoCircle } from "@fortawesome/free-solid-svg-icons";
+import { faGithub } from "@fortawesome/free-brands-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
+const Card = (props) => {
+  console.log(props.video);
 
   return (
     <section className="projects-section">
-    <div className="card-menu" id="projects">
-      <h1 className="projects-name">{props.name}</h1>
-      <p className="description">{props.description}</p>
-      <a href="https://eloquent-sammet-a7d128.netlify.app" target="_blank"><img className="project-gif" src={WeatherApp} /></a>
-      
-      <ol className="tech-use">
-        {/* Iterrate over the array return a list item */}
-        {
-          (props.techUse).map((tech) =>
-          {
-            return(<li>{tech}</li>)
-        })
-       }
-      
-      </ol>
-      <a href="https://github.com/alexcruz097/weather-react" target="_blank"><img className="gitHub"  /></a>
+      <div className="card-menu" id="projects">
+        <h1 className="projects-name">{props.name}</h1>
+        <FontAwesomeIcon className="info-icon" icon={faInfoCircle} />
+        <p className="description">{props.description}</p>
+        <a href="https://eloquent-sammet-a7d128.netlify.app" target="_blank">
+          <img className="project-gif" src={WeatherApp} />
+        </a>
+        <ol className="tech-use">
+          {/* Iterrate over the array return a list item */}
+          {props.techUse.map((tech) => {
+            return <li>{tech}</li>;
+          })}
+        </ol>
+        <a href="https://github.com/alexcruz097/weather-react" target="_blank">
+          <FontAwesomeIcon icon={faGithub} size="3x" />
+        </a>
       </div>
-      </section>
+    </section>
   );
 };
 
